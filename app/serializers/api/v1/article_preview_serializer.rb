04694958ff40de4ae5431120d362ed732532c034
服部元志 <hatowork.7979@gmail.com>
2023-11-23 21:06:08 +0900

    no message

--- app/serializers/api/v1/article_preview_serializer.rb
+++ app/serializers/api/v1/article_preview_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::ArticlePreviewSerializer < ActiveModel::Serializer
-  #index
-  attributes  :id, :title, :updated_at
+  # index
+  attributes :id, :title, :updated_at
   belongs_to :user, serializer: Api::V1::UserSerializer
 end
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,46 +1,47 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "Articles", type: :request do
-
-  #index
+RSpec.describe "Articles" do
+  # index
   describe "GET /api/v1/articles" do
-    subject {get(api_v1_articles_path)}
+    subject { get(api_v1_articles_path) }
+
     before {
       user = create(:user)
-      3.times{create(:article,user_id: user.id )}
+      3.times { create(:article, user_id: user.id) }
     }
+
     it "記事一覧を閲覧できる" do
       subject
 
       # binding.pry
-      res = JSON.parse(response.body)
+      res = response.parsed_body
       expect(res.length).to eq 3
       expect(res[0].keys).to eq ["title", "updated_at"]
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 
-  #show
-  describe "GET /api/v1/articles/:id" do
-    it "任意の記事が閲覧できる" do
-    end
-  end
+  # # show
+  # describe "GET /api/v1/articles/:id" do
+  #   it "任意の記事が閲覧できる" do
+  #   end
+  # end
 
-  #create
-  describe "POST /api/v1/articles" do
-    it "記事を新規作成できる" do
-    end
-  end
+  # # create
+  # describe "POST /api/v1/articles" do
+  #   it "記事を新規作成できる" do
+  #   end
+  # end
 
-  #update
-  describe "PATCH /api/v1/articles/:id" do
-    it "任意の記事を更新できる" do
-    end
-  end
+  # # update
+  # describe "PATCH /api/v1/articles/:id" do
+  #   it "任意の記事を更新できる" do
+  #   end
+  # end
 
-  #DELETE
-  describe "DELETE /api/v1/articles/:id " do
-    it "任意の記事を削除できる" do
-    end
-  end
+  # # DELETE
+  # describe "DELETE /api/v1/articles/:id" do
+  #   it "任意の記事を削除できる" do
+  #   end
+  # end
 end
