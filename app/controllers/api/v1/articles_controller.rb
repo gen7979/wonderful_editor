34875a94e1dd27ace511506baee98d8a891bee1a
服部元志 <hatowork.7979@gmail.com>
2023-11-23 21:02:08 +0900

    記事一覧機能の実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
-    @articles = Article.all
-    render json: @articles
+    articles = Article.order(updated_at: :desc)
+    render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
   end
 end
