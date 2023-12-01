5a3af1ba46737f86f7121f02ba92ae123511cff4
服部元志 <hatowork.7979@gmail.com>
2023-12-01 23:11:08 +0900

    createメソッドを定義

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::ArticlesController < ApplicationController
+class Api::V1::ArticlesController < Api::V1::BaseApiController
   def index
     articles = Article.order(updated_at: :desc)
     render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
@@ -8,4 +8,15 @@ class Api::V1::ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     render json: @article, serializer: Api::V1::ArticleSerializer
   end
+
+  def create
+    article = current_user.articles.create!(article_params)
+    render json: article, serializer: Api::V1::ArticleSerializer
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -49,10 +49,24 @@ RSpec.describe "Articles" do
   end
 
   # # create
-  # describe "POST /api/v1/articles" do
-  #   it "記事を新規作成できる" do
-  #   end
-  # end
+  describe "POST /articles" do
+    subject { post(api_v1_articles_path, params: params) }
+
+    let(:params) { { article: attributes_for(:article) } }
+    let(:current_user) { create(:user) }
+
+    # stub
+    before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+
+    it "記事のレコードが作成できる" do
+      expect { subject }.to change { Article.where(user_id: current_user.id).count }.by(1)
+
+      res = response.parsed_body
+      expect(res["title"]).to eq params[:article][:title]
+      expect(res["body"]).to eq params[:article][:body]
+      expect(response).to have_http_status(:ok)
+    end
+  end
 
   # # update
   # describe "PATCH /api/v1/articles/:id" do
