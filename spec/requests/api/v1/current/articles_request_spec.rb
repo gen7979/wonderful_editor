1ee4abd22c066090e2d32bdf5e48fe797b14cec4
服部元志 <hatowork.7979@gmail.com>
2024-01-29 00:23:22 +0900

    マイページの実装

---
+++ spec/requests/api/v1/current/articles_request_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "Api::V1::Current::Articles" do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/api/v1/current/articles/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/api/v1/current/articles/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
